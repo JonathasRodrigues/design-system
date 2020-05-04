34c8cf3f1adf4ef8b5ce90584436fa7e4ef3ac8d
Jonathas Rodrigues <jonathas.rodrigues@eduzz.com>
2020-05-04 12:33:14 -0300

    add typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ module.exports = {
       exclude: /^@babel/
     }
   }],
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx', '../src/**/*.stories.ts'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-docs',
@@ -13,4 +13,20 @@ module.exports = {
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
   ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import * as styles from './shared/styles';
+import * as global from './shared/global';
+import * as animation from './shared/animation';
+
+export { styles, global, animation };
